73037b863203224bf7fe60c1c0cd52d485b25fbb
gringrape <gringrape200@gmail.com>
2025-05-07 09:49:35 +0900

    안먹는재료 페이지 스타일

--- src/pages/ExcludeIngredientsPage.js
+++ src/pages/ExcludeIngredientsPage.js
@@ -2,15 +2,41 @@ import { useState } from 'react';
 
 import { useNavigate } from 'react-router';
 
+import styled from 'styled-components';
+
+import Page from '../components/Page';
+import Submit from '../components/Submit';
+
+const Select = styled.div`
+  width: 100%;
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 1em;
+`;
+
+const SelectButton = styled.button`
+  font-size: ${({ theme }) => theme.typography.h3.fontSize};
+  display: block;
+  width: 100%;
+  padding: 1.5em 0;
+  border-radius: 0.75em;
+  border: 1px solid ${({ theme, selected }) => selected ? theme.colors.primary : theme.colors.state.disabled};
+  color: ${({ theme, selected }) => selected ? theme.colors.primary : theme.colors.text};
+  background-color: ${({ theme, selected }) => selected ? theme.colors.primarySuperLight : theme.colors.background};
+  font-weight: bold;
+`;
+
+const ingredients = [
+  { id: 'eggs', name: '🥚 달걀' },
+  { id: 'beans', name: '🥜 콩/견과류' },
+  { id: 'meat', name: '🐓 가금류' },
+  { id: 'fish', name: '🐟 어패류' },
+  { id: 'fruit', name: '🍉 과일' },
+];
+
 function ExcludeIngredientsPage() {
   const navigate = useNavigate();
   const [selectedIngredients, setSelectedIngredients] = useState([]);
-
-  const ingredients = [
-    { id: 'gluten', name: '글루텐' },
-    { id: 'soy', name: '대두' },
-    { id: 'nuts', name: '견과류' }
-  ];
 
   const handleIngredientChange = (ingredientId) => {
     setSelectedIngredients(prev => {
@@ -27,29 +53,27 @@ function ExcludeIngredientsPage() {
   };
 
   return (
-    <div>
-      <h1>제외할 재료 선택</h1>
-      <div>
-        {ingredients.map((ingredient) => (
-          <div key={ingredient.id}>
-            <input
-              type="checkbox"
-              id={ingredient.id}
-              checked={selectedIngredients.includes(ingredient.id)}
-              onChange={() => handleIngredientChange(ingredient.id)}
-            />
-            <label htmlFor={ingredient.id}>
+    <Page>
+      <form>
+        <label>안먹는 재료가 있나요?</label>
+        <p>정확한 식단추천을 위해 제외할 재료를 모두 선택해주세요</p>
+        <Select>
+          {ingredients.map((ingredient) => (
+            <SelectButton
+              key={ingredient.id} 
+              type="button"
+              selected={selectedIngredients.includes(ingredient.id)}
+              onClick={() => handleIngredientChange(ingredient.id)}
+            >
               {ingredient.name}
-            </label>
-          </div>
-        ))}
-      </div>
-      <div>
-        <button onClick={handleNext}>
-          다음
-        </button>
-      </div>
-    </div>
+            </SelectButton>
+          ))}
+        </Select>
+      </form>
+      <Submit onClick={handleNext}>
+        다음
+      </Submit>
+    </Page>
   );
 }
 
